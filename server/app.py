c0906b3f53f4e2b43cdc078f6fc4c26d18415960
Captainsan90 <captainsannn@gmail.com>
2026-04-16 03:21:14 +0300

    Car Routes

--- server/app.py
+++ server/app.py
@@ -1,1 +1,21 @@
-existing_models = ['Beedle', 'Crossroads', 'M2', 'Panique']
+from flask import Flask
+
+app = Flask(__name__)
+
+# Default route
+@app.route('/')
+def index():
+    return "Welcome to Flatiron Cars"
+
+
+# Model-specific route
+@app.route('/<model>')
+def car_model(model):
+    if model == "Crossroads":
+        return f"Flatiron {model} is in our fleet!"
+    else:
+        return f"No models called {model} exists in our catalog"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
